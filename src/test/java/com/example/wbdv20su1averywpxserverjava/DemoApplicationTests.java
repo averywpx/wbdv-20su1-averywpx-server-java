1567954edb6b7827e6a54d162dccb5585cfa20d9
averywpx <avery.wpx@gmail.com>
2020-06-16 10:47:02 -0400

    initialize

---
+++ src/test/java/com/example/wbdv20su1averywpxserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdv20su1averywpxserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
